eff72051f17017e5be5d3b573fb8692e2df9ee4d
sandraG95 <118214786+sandraG95@users.noreply.github.com>
2022-12-06 22:14:50 +0100

    Update main.java
    hola hola

--- prueba/src/prueba/main.java
+++ prueba/src/prueba/main.java
@@ -3,7 +3,7 @@ package prueba;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
+		System.out.println("hola que tal estoy intentando hacer esto);
 
 	}
 
